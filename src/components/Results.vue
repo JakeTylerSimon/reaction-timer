9b6022d2ffa4d8929a25498417cd0567c6c4453d
JakeSimon <Jaketylersimon@gmail.com>
2022-09-28 15:25:24 -0700

    finish

---
+++ src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+    <p>Reaction Time: {{score}} ms</p>
+    <p class="rank">{{rank}}</p>
+</template>
+
+<script>
+    export default {
+        props: ['score',],
+        data() {
+            return {
+                rank: null,
+            }
+        },
+        mounted() {
+            if(this.score < 350) {
+                this.rank = 'Elite Status'
+            } else if(this.score < 450) {
+                this.rank = 'Quick with it'
+            } else {
+                this.rank = 'Come onnn, you can do better'
+            }
+        }
+    }
+</script>
+
+<style>
+    .rank {
+        font-size: 1.4rem;
+        color: teal;
+        font-weight: bold;
+    }
+</style>
